57e1522026dfbe76b69811e0b1104151307c1d05
Eric Biewener <ebiewener@gmail.com>
2019-04-02 23:55:12 -0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import { WebClient } from '@slack/web-api'
+import chrono from 'chrono-node'
+// @ts-ignore
+import log from 'log-all-the-things'
+
+const userEric = 'UANJ8LMLL'
+
+const web = new WebClient(process.env.SLACK_TOKEN);
+
+export async function createReminder(str: string) {
+  const results = chrono.parse(str)
+  if (results.length !== 1) {
+    log.e('Did not find exactly 1 result.')
+    log.e(results)
+    return
+  }
+
+  const time = results[0].text;
+  const text = str.replace(time, '').replace(/\s\s+/g, ' ').trim()
+  
+  if (!text) {
+    log.e('No message text.')
+    return
+  }
+
+  const result = await web.reminders.add({
+    user: userEric,
+    time,
+    text,
+  })
+
+  log.s(result)
+}
+
+const args = process.env.npm_config_argv as string
+const str = JSON.parse(args).original.slice(1).join(' ')
+
+if (!str) {
+  log.e('No text provided.')
+  process.exit(0)
+}
+
+createReminder(str)
+
